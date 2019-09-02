340ea0d070f637d2f447001e5fbfbf5098437f3b
AqeeAqee <aqee@msn.com>
2019-09-03 01:24:10 +0800

    init

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
- 
+// Add your code here
+//% color="#eeaaaa"
+namespace broadcast {
+
+    export interface BroadcastHandler {
+        msg: string;
+        handler: () => void;
+    }
+
+    let broadcastHandlers: BroadcastHandler[];
+
+
+    /**
+     * Run following code, when received the same Message String
+     */
+    //% block="when I receive $msg"
+    export function onEventWithArgs(msg: string, handler: () => void) {
+        if (broadcastHandlers == null)
+            broadcastHandlers = [];
+        broadcastHandlers.push({ msg: msg, handler: handler });
+    }
+
+    /**
+     * Send out Broadcast with Message String
+     */
+    //% block="broadcast %msg"
+    export function broadcast(msg: string) {
+        broadcastHandlers
+            .filter(h => h.msg == msg)
+            .forEach(h => h.handler());
+
+    }
+
+}
